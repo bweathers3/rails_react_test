562193f690fb1894acd8a4034230830e98fdab5a
Bailey <bailey.g.weathers@gmail.com>
2020-02-16 23:27:58 -0500

    Add bootstrap

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export default (props) => (
+  <div className="vw-100 vh-25 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Welcome to the Church Bulletin App</h1>
+        <p className="lead align-items-center">
+          This app is designed to keep you up to date with events in your church.
+        </p>
+        <p className="lead"></p>
+        <p className="lead">
+          In addition this app gives you an enviorment to take notes during worship time.
+        </p>
+        <hr className="my-4" />
+        <Link
+          to="/churchhomepage"
+          className="btn btn-lg btn-dark"
+          role="button"
+        >
+          Church Home Page
+        </Link>
+
+      </div>
+    </div>
+  </div>
+);
